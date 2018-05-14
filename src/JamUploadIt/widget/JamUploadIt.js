ca130730b74d33d35f120f3814c6636cbc9fe646
Martijn Raats <martijn.raats@jam-it.nl>
2018-05-14 19:11:40 +0200

    SIM-172 Bijlagen uploader (vergaderingen) werkt niet

--- src/JamUploadIt/widget/JamUploadIt.js
+++ src/JamUploadIt/widget/JamUploadIt.js
@@ -75,7 +75,6 @@ define([
 
         // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
         constructor: function () {
-            logger.level(logger.DEBUG);
             logger.debug(this.id + ".constructor");
             this._handles = [];
         },
